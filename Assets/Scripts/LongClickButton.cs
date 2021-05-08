8c8e4b6516166bec06b216fa689e16f77dc58688
MadMonk <mmajewski333@gmail.com>
2021-05-08 16:06:36 +0200

    Added Wheel Of Fortune feature
    -

---
+++ Assets/Scripts/LongClickButton.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown;
+   // private bool pointerUp;
+
+    public float maxFillTime;
+    public float pointerDownTimer;
+
+    public UnityEvent onLongClick;
+
+    [SerializeField]
+    private Image fillImage;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if(pointerDownTimer == maxFillTime)
+            {
+                pointerDownTimer -= Time.deltaTime;
+            }
+
+            fillImage.fillAmount = pointerDownTimer / maxFillTime;
+        }
+
+        if(!pointerDown)
+        {
+            onLongClick.Invoke();
+            Debug.Log("Odpalam funkcje");
+        }
+        
+    }
+
+    //void Update()
+    //{
+    //    if (pointerDown)
+    //    {
+    //        pointerDownTimer += Time.deltaTime;
+    //        if (pointerDownTimer >= maxFillTime)
+    //        {
+    //            if (onLongClick != null)
+    //                onLongClick.Invoke();
+
+    //            Reset();
+    //        }
+
+    //        fillImage.fillAmount = pointerDownTimer / maxFillTime;
+    //    }
+    //}
+}
